d11589b357dea3db46ff22a0818f3110daa02c56
Alexandre Perrin <alex@kaworu.ch>
2014-01-15 11:15:00 +0100

    Update lua_sysctl.c
    happy new year.

--- src/lua_sysctl.c
+++ src/lua_sysctl.c
@@ -12,7 +12,7 @@
  *
  * Copyright (c) 1993
  *    The Regents of the University of California.  All rights reserved.
- * Copyright (c) 2008-2013
+ * Copyright (c) 2008-2014
  *    Alexandre Perrin <alex@kaworu.ch>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
