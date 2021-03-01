8ac0ae556395d382efa89b6451c9db33911a5589
Alex Samorukov <samm@os2.kiev.ua>
2021-03-01 02:00:57 +0100

    Use lua_pushinteger instead of lua_pushunsigned
    Use lua_pushinteger instead of lua_pushunsigned to make it compatible with lua 53+. Tested on LUA 5.3 and LUA 5.4

--- src/lua_sysctl.c
+++ src/lua_sysctl.c
@@ -517,7 +517,7 @@ luaA_sysctl_get(lua_State *L)
 				if (intlen > sizeof(lua_Unsigned))
 					lua_pushnumber(L, umv);
 				else
-					lua_pushunsigned(L, umv);
+				        lua_pushinteger(L, (lua_Integer)(umv));
 			}
 			lua_settable(L, -3);
 			len -= intlen;
